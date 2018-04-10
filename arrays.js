d70ca482def2af99ee16806ccff7ff933c6564c3
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-10 17:08:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
